c7132b360adcc64f93cdb3d532e623719a6a28b1
Alekhya <NAlekhya.java@gmail.com>
2025-03-19 19:54:16 -0500

    Added Account class

---
+++ src/main/java/com/bofa/model/Accounts.java
@@ -0,0 +1,12 @@
+package com.bofa.model;
+import java.time.LocalDateTime;
+
+public class Accounts {
+
+	public long accountId;
+	public long accountNumber;
+	public String accountType;
+	private long balance;
+	public LocalDateTime createdDate;
+	public boolean status;
+}
