a32d1596d32112c36c5b8507128909b527efb3a8
gt0212-gowtham <gowthamth599@gmail.com>
2025-03-20 11:52:56 -0500

    updated both employees and Testemployee classes

--- src/main/java/com/bofa/model/Employees.java
+++ src/main/java/com/bofa/model/Employees.java
@@ -1,4 +1,106 @@
 package com.bofa.model;
 
+import lombok.ToString;
+
 public class Employees {
+//     *  Employees -- Gowth
+
+     public String employeeId;
+     private String firstName;
+     private String lastName;
+     private String address;
+     private String email;
+     private long mobileNumber;
+     private String gender;
+     private long  salary;
+     public Employees() {}
+
+     public Employees(String employeeId, String firstName, String lastName, String address, String email, long mobileNumber, String gender, long salary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.gender = gender;
+        this.salary = salary;
+     }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employees{" +
+                "employeeId='" + employeeId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", mobileNumber=" + mobileNumber +
+                ", gender='" + gender + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
