a691265f0d98bdb4961dde25bebfea40626b968c
BukkaRamesh <bukkaramesh.b@gmail.com>
2025-03-19 21:07:35 -0500

    added new test

--- src/main/java/com/bofa/model/Customer.java
+++ src/main/java/com/bofa/model/Customer.java
@@ -12,10 +12,29 @@ public class Customer {
 	 * 
 	 */
 	public Customer() {
-		// TODO Auto-generated constructor stub
+		System.out.println("the default const is called");
 	}
-
+	
+	public Customer(long customerId) {
+		this.customerId = customerId;
+		System.out.println("the customerid const is called");
+	}
+	
+	public Customer(String firstName) {
+		
+	}
+	
+	
 	/*
+	 *  Constructor will have same class name
+	 *  you can have default const
+	 *  and parametrized construct
+	 *  you can have n no of constr
+	 *  
+	 *  
+	 *  the constructors are useful while you create an object 
+	 * 
+	 * 
 	 * Data types
 	 * 
 	 * Integer -- 1 -- 10mb Long -- 123144 -- 100mb Boolean -- true /false -- String
@@ -35,6 +54,36 @@ public class Customer {
 	 *   protected
 	 *   default   --
 	 *   
+	 *   
+	 *   Varaibles 
+	 *   
+	 *   local 
+	 *   global
+	 *   
+	 *   
+	 *   method 
+	 *   
+	 *   syntax: 
+	 *   accessspecifier returntype methodname(parameters/method signiture){
+	 *   method body
+	 *   }
+	 *   
+	 *   the retyurn types can be data types or objects, entities 
+	 *   void -- will not expect anything 
+	 *   
+	 *   
+	 *   // operators 
+	 *   arithmetic operatiors   // +, -, *, /, %, ++, --
+	 *   assignment    //   = , += , 
+	 *   relational    // == , !=, >, <, <=, >= 
+	 *   logic operators  && , ||
+	 *   
+	 *     bitwise 1 = 0001
+	 *     int a = ++1
+	 *     
+	 *     c += a 
+	 *     c = c + a //  both 
+	 *     
 	 * 
 	 * 
 	 */
@@ -54,5 +103,34 @@ public class Customer {
 	private String gender;
 	
 	private boolean status;
+	
+	static String name = "Alex";  // variable the scope of this varaible is there till entire class because you made it as globa; variable 
+	
+	
+	
+	public static int calc(int a, int b ) {
+	  a = 1;
+	  b = 2;
+	  
+	  int c = a+b;    // variable  the scope of varaibale c is exists only insied calac method
+	  
+	  
+	  
+	  System.out.println(c);
+		
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		int a=0, b=0, x=1;
+		
+		calc(a,b);   // output
+		
+		  System.out.println(name);
+		
+	}
+	
 
 }
