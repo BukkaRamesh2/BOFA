da28d0f8f5d6fad13f5dda7fd609a97104c2ac23
Alekhya <NAlekhya.java@gmail.com>
2025-03-24 13:15:06 -0500

    Added accounts

--- src/main/java/com/bofa/model/Accounts.java
+++ src/main/java/com/bofa/model/Accounts.java
@@ -9,4 +9,24 @@ public class Accounts {
 	private long balance;
 	public LocalDateTime createdDate;
 	public boolean status;
+	
+	public Accounts(long accountId, long accountNumber, String accountType, long balance, boolean status) {
+        this.accountId = accountId;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.createdDate = LocalDateTime.now();
+        this.status = status;
+    }
+
+
+public void main(String[] args) {
+   //Accounts account1 = new Accounts(001, 987654321, "Savings", 5000, true);
+   //Accounts account2 = new Accounts(002, 123456789, "Checking", 3000, true);
+	accountId = 001;
+	accountNumber = 123456789;
+	accountType = "savings";
+	balance = 10000;
+	status = true;
+}
 }
