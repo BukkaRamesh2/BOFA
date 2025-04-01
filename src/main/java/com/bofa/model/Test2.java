01c7bf59ea3045d40fd6c0bcbae3b84e192f742d
srijan1151 <srijangone11@gmail.com>
2025-04-01 17:16:19 -0400

    Added conditional Stmts, Switch Cases, Controller Class, Exception Handling and Transaction Service

--- src/main/java/com/bofa/model/Test2.java
+++ src/main/java/com/bofa/model/Test2.java
@@ -4,14 +4,14 @@ import com.bofa.model.Transaction;
 
 public class Test2 {
     public static void main(String[] args) {
-        // Creating a Transaction object using the parameterized constructor
+        
         Transaction transaction1 = new Transaction(101, "Deposit", 5000.00, "BR001", 10000.00);
         
-        // Displaying the transaction details
+       
         System.out.println("Transaction 1 Details:");
         System.out.println(transaction1);
 
-        // Creating a Transaction object using the no-argument constructor
+        
         Transaction transaction2 = new Transaction();
         transaction2.setTransactionId(102);
         transaction2.setTransactionType("Withdrawal");
@@ -19,11 +19,11 @@ public class Test2 {
         transaction2.setBranchCode("BR002");
         transaction2.setBalance(8000.00);
 
-        // Displaying the second transaction details
+       
         System.out.println("\nTransaction 2 Details:");
         System.out.println(transaction2);
         
-        // Testing getter methods
+  
         System.out.println("\nTesting Getters:");
         System.out.println("Transaction ID: " + transaction2.getTransactionId());
         System.out.println("Transaction Type: " + transaction2.getTransactionType());
